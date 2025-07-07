fa10142be9b2714626152e9eae5017976f1a157a
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-07 12:30:19 +0200

    Initialization du projet Spring Boot

---
+++ IVR-api/src/test/java/com/_CServices/IVR_api/IvrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._CServices.IVR_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
