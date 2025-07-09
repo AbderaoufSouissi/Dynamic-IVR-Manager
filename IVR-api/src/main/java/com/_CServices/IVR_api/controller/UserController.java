2ec4ef1b2cb3d46ed3677899332dedcd1945543d
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-09 13:23:41 +0200

    Backend: Ajout de la couche controller de l'entité User

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/controller/UserController.java
@@ -0,0 +1,77 @@
+package com._CServices.IVR_api.controller;
+
+import com._CServices.IVR_api.dto.UserDto;
+import com._CServices.IVR_api.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<List<UserDto>> getUsersByActiveStatus(@RequestParam boolean active) {
+        return  new ResponseEntity<>(userService.getUsersByActiveStatus(active), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<UserDto> getUserByEmail(@RequestParam String email) {
+        return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/username")
+    public ResponseEntity<UserDto> getUserByUsername(@RequestParam String username) {
+        return new ResponseEntity<>(userService.getUserByUsername(username), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
+        return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/email")
+    public ResponseEntity<Void> deleteUserByEmail(@RequestParam String email) {
+        userService.deleteUserByEmail(email);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/username")
+    public ResponseEntity<Void> deleteUserByUsername(@RequestParam String username) {
+        userService.deleteUserByUsername(username);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUserById(@PathVariable Long id, @RequestBody @Valid UserDto userDto) {
+        return new ResponseEntity<>(userService.updateUser(userDto, id), HttpStatus.OK);
+    }
+
+
+}
