c4fd957aa522c5fd61140a9f016ebbc7a90f7684
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-09 16:19:09 +0200

    Backend: Ajout de la vérification de l'état du compte User

--- IVR-api/src/main/java/com/_CServices/IVR_api/service/impl/AuthServiceImpl.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/service/impl/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com._CServices.IVR_api.service.impl;
 
 import com._CServices.IVR_api.dao.UserRepository;
 import com._CServices.IVR_api.dto.request.LoginRequest;
+import com._CServices.IVR_api.entity.User;
 import com._CServices.IVR_api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +33,14 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public String login(LoginRequest request) {
         log.info("inside login()");
+        User existingUser = userRepository.findByUsername(request.getUsername());
+        if (null != existingUser) {
+
+            Boolean isActive = existingUser.getActive();
+            if (!isActive) {
+                return "User: "+existingUser.getUsername()+" is not active";
+            }
+        }
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
