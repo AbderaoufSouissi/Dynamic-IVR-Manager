df96ef8137b978d35fab306a19c91651a04ffe5c
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-09 12:03:07 +0200

    feat(backend): ajout d'exceptions personnalisées et du gestionnaire global:

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com._CServices.IVR_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
