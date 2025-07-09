0f71f988ad62a4a6e685f60ff46ada4a0188b1b5
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-09 10:36:32 +0200

    Backend: Ajouter UserMapper pour mapper entre DTO et entité JPA

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com._CServices.IVR_api.mapper;
+
+import com._CServices.IVR_api.dto.UserDto;
+import com._CServices.IVR_api.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserDto toDto(User user) {
+        return UserDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+
+    }
+}
