b5c523e532996cef7c23b0f0152ae2875853f75e
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-09 10:33:41 +0200

    Backend: Ajout de l'annotation @EnableJpaAuditing pour activer l'audit

--- IVR-api/src/main/java/com/_CServices/IVR_api/IvrApiApplication.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/IvrApiApplication.java
@@ -2,8 +2,11 @@ package com._CServices.IVR_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IvrApiApplication {
 
 	public static void main(String[] args) {
