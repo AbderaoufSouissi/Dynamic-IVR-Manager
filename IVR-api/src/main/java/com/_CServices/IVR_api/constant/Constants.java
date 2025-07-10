86bfebed9c60d344fc8be3608d8dc5b000bab18a
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-10 15:45:12 +0200

    Backend: Ajout de la classe Constants qui contient les descriptions des differentes permissions

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/constant/Constants.java
@@ -0,0 +1,13 @@
+package com._CServices.IVR_api.constant;
+
+public class Constants {
+    public static final String CREATE_USER_PERMISSION="Creér un utilisateur";
+    public static final String UPDATE_USER_PERMISSION="Mettre à jour un utilisateur";
+    public static final String DELETE_USER_PERMISSION="Supprimer un utilisateur";
+    public static final String CREATE_ROLE_PERMISSION="Creér un role";
+    public static final String UPDATE_ROLE_PERMISSION="Mettre à jour un role";
+    public static final String DELETE_ROLE_PERMISSION="Supprimer un role";
+    public static final String BLACKLIST_CUSTOMER_PERMISSION="Blacklister un client";
+    public static final String WHITELIST_CUSTOMER_PERMISSION="Whitelister un client";
+    public static final String RESET_NB_CALLS_PERMISSION="Réinitialiser le nombre d'appels d'un client";
+}
