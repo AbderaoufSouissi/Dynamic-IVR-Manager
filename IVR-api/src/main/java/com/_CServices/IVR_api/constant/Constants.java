ad606258b194f1afa5d743b6022eb3132ca539b6
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-11 16:53:36 +0200

    Backend: Modification de la classe Constants

--- IVR-api/src/main/java/com/_CServices/IVR_api/constant/Constants.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/constant/Constants.java
@@ -4,9 +4,14 @@ public class Constants {
     public static final String CREATE_USER_DESCRIPTION="Creér un utilisateur";
     public static final String UPDATE_USER_DESCRIPTION="Mettre à jour un utilisateur";
     public static final String DELETE_USER_DESCRIPTION="Supprimer un utilisateur";
+
     public static final String CREATE_ROLE_DESCRIPTION="Creér un role";
     public static final String UPDATE_ROLE_DESCRIPTION="Mettre à jour un role";
     public static final String DELETE_ROLE_DESCRIPTION="Supprimer un role";
+
+    public static final String CREATE_PERMISSION_DESCRIPTION="Creér une permission";
+    public static final String DELETE_PERMISSION_DESCRIPTION="Supprimer une permission";
+
     public static final String BLACKLIST_CUSTOMER_DESCRIPTION="Blacklister un client";
     public static final String WHITELIST_CUSTOMER_DESCRIPTION="Whitelister un client";
     public static final String RESET_NB_CALLS_DESCRIPTION="Réinitialiser le nombre d'appels d'un client";
