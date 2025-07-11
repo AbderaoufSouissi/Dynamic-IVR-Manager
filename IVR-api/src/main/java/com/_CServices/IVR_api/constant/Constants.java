ff162d7f5ad504697b9685aa93018c9488d2c43b
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-11 12:56:56 +0200

    Backend: Modification de la classe Constants et de l'enum ActionType

--- IVR-api/src/main/java/com/_CServices/IVR_api/constant/Constants.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/constant/Constants.java
@@ -1,13 +1,13 @@
 package com._CServices.IVR_api.constant;
 
 public class Constants {
-    public static final String CREATE_USER_PERMISSION="Creér un utilisateur";
-    public static final String UPDATE_USER_PERMISSION="Mettre à jour un utilisateur";
-    public static final String DELETE_USER_PERMISSION="Supprimer un utilisateur";
-    public static final String CREATE_ROLE_PERMISSION="Creér un role";
-    public static final String UPDATE_ROLE_PERMISSION="Mettre à jour un role";
-    public static final String DELETE_ROLE_PERMISSION="Supprimer un role";
-    public static final String BLACKLIST_CUSTOMER_PERMISSION="Blacklister un client";
-    public static final String WHITELIST_CUSTOMER_PERMISSION="Whitelister un client";
-    public static final String RESET_NB_CALLS_PERMISSION="Réinitialiser le nombre d'appels d'un client";
+    public static final String CREATE_USER_DESCRIPTION="Creér un utilisateur";
+    public static final String UPDATE_USER_DESCRIPTION="Mettre à jour un utilisateur";
+    public static final String DELETE_USER_DESCRIPTION="Supprimer un utilisateur";
+    public static final String CREATE_ROLE_DESCRIPTION="Creér un role";
+    public static final String UPDATE_ROLE_DESCRIPTION="Mettre à jour un role";
+    public static final String DELETE_ROLE_DESCRIPTION="Supprimer un role";
+    public static final String BLACKLIST_CUSTOMER_DESCRIPTION="Blacklister un client";
+    public static final String WHITELIST_CUSTOMER_DESCRIPTION="Whitelister un client";
+    public static final String RESET_NB_CALLS_DESCRIPTION="Réinitialiser le nombre d'appels d'un client";
 }
