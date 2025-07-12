9291551c0533aedff487220eeb586d407eebe414
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-07-12 11:33:28 +0100

    Backend: Modification de la classe RoleDto

--- IVR-api/src/main/java/com/_CServices/IVR_api/dto/RoleDto.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/dto/RoleDto.java
@@ -1,5 +1,6 @@
 package com._CServices.IVR_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.Set;
 @Builder
 public class RoleDto {
     private Long id;
+    @NotBlank(message = "Role name is required")
     private String name;
     @Builder.Default
     private Set<String> Permissions = new HashSet<>();
