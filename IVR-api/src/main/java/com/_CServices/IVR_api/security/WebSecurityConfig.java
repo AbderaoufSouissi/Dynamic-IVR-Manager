0c162b7ef83ae660fce1d1a636af1293a2cdc4c9
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-14 11:05:51 +0200

    Backend: Modification de la classe WebSecurityConfig.java

--- IVR-api/src/main/java/com/_CServices/IVR_api/security/WebSecurityConfig.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/security/WebSecurityConfig.java
@@ -1,65 +1,53 @@
-    package com._CServices.IVR_api.security;
+package com._CServices.IVR_api.security;
 
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.http.HttpStatus;
-    import org.springframework.security.authentication.AuthenticationManager;
-    import org.springframework.security.config.Customizer;
-    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.config.http.SessionCreationPolicy;
-    import org.springframework.security.core.session.SessionRegistry;
-    import org.springframework.security.core.session.SessionRegistryImpl;
-    import org.springframework.security.core.userdetails.UserDetailsService;
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-    import org.springframework.security.crypto.password.PasswordEncoder;
-    import org.springframework.security.web.SecurityFilterChain;
-    import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-    import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-    import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-    import static org.springframework.security.config.http.SessionCreationPolicy.IF_REQUIRED;
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
 
-    @EnableWebSecurity
-    @Configuration
-    @RequiredArgsConstructor
-    public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
 
-        private final CustomUserDetailsService userDetailsService;
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            return http
-                    .csrf(csrf -> csrf.disable())
-//                    .headers(headers -> headers.frameOptions(frame -> frame.disable())) // ← allow frames
-                    .authorizeHttpRequests(auth -> auth.requestMatchers("/h2-console/**").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .httpBasic(Customizer.withDefaults())
-                    .userDetailsService(userDetailsService)
-                    .build();
-        }
-
-
-
-        @Bean
-        public SessionRegistry sessionRegistry() {
-            return new SessionRegistryImpl();
-        }
-
-
-        @Bean
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-            return authConfig.getAuthenticationManager();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService)
+                .build();
     }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
