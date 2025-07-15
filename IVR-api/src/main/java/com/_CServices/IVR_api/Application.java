a775edc213b67471d47d0ceb6047e6b0d66e7eed
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-15 15:52:54 +0200

    Backend: Ajout de l'annotation @EnabelScheduling à la classe Application.java

--- IVR-api/src/main/java/com/_CServices/IVR_api/Application.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/Application.java
@@ -1,9 +1,10 @@
 package com._CServices.IVR_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-
 public class Application {
 
 	public static void main(String[] args) {
