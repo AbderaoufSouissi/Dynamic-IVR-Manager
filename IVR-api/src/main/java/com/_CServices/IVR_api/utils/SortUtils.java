d384619becc62a7d82bd2051672868a4a62c3dee
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-17 11:35:38 +0200

    Backend: Implémentation du filtrage et de la pagination pour Audit
    - Ajout des paramètres de filtrage : id, userId, date, entity, action
    - Mise en place de la pagination pour les résultats
    - Optimisation des requêtes vers la base de données

--- IVR-api/src/main/java/com/_CServices/IVR_api/utils/SortUtils.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/utils/SortUtils.java
@@ -8,7 +8,7 @@ public class SortUtils {
     private static final Set<String> ALLOWED_USER_SORT_FIELDS = Set.of("user_id", "username", "email", "first_name", "last_name", "is_active", "created_at", "updated_at");
     private static final Set<String> ALLOWED_ROLE_SORT_FIELDS = Set.of("role_id", "role_name", "created_at", "updated_at");
     private static final Set<String> ALLOWED_PERMISSION_SORT_FIELDS = Set.of("permission_id", "permission_name");
-    private static final Set<String> ALLOWED_AUDIT_SORT_FIELDS = Set.of("audit_id", "user_id", "created_at", "updated_at");
+    private static final Set<String> ALLOWED_AUDIT_SORT_FIELDS = Set.of("audit_id", "user_id", "action_time_stamp", "action_type","entity_type","entity_id","msisdn");
 
     public static String sanitizeSortField(String input, Set<String> allowedFields, String defaultField) {
         return allowedFields.contains(input.toLowerCase()) ? input : defaultField;
