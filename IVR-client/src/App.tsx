8e79cb23d8dbf7b5f4565be2100f7a5db9738688
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-18 17:15:12 +0200

    Frontend: Ajout de la configuration de routage initiale

--- IVR-client/src/App.tsx
+++ IVR-client/src/App.tsx
@@ -1,8 +1,20 @@
 
+import AdminDashboard from "./pages/AdminDashboard";
+import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import LoginPage from "./pages/LoginPage";
+
+
+
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      {/* <ForgetPasswordPage/> */}
+      {/* <LoginPage/> */}
+      <AdminDashboard/>
+    </>
   )
 }
 
-export default App
+export default App;
--- IVR-client/src/main.tsx
+++ IVR-client/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.tsx'
+import ForgetPasswordPage from './pages/ForgetPasswordPage.tsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: createElement(App)
+  },
+  
+  {
+    path: "/login",
+    element: createElement(LoginPage)
+  },
+  
+  {
+    path: "/forget-password",
+    element: createElement(ForgetPasswordPage)
+  },
+  
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
