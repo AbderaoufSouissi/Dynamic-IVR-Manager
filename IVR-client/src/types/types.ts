466b237c1fb49d7b6722aa95e29b0c2e17427427
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-18 15:40:49 +0200

    Frontend: Ajout d'un fichier types.ts qui definit les types User, Role, Permission, Audit

---
+++ IVR-client/src/types/types.ts
@@ -0,0 +1,44 @@
+export interface User {
+    userId: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    roleName: string;
+    createdAt: string;
+    createdBy: string;
+    updatedAt: string;
+    updatedBy: string;
+    active: boolean;
+
+}
+
+
+export interface Role {
+    roleId: number
+    name: string
+    permissions: string[]
+    createdAt: string; 
+    createdBy: string
+    updatedAt: string;
+    updatedBy: string;
+}
+
+export interface Permission {
+    permissionId: number
+    name: string
+    description: string
+    createdAt: string; 
+    createdBy: string
+    updatedAt: string;
+    updatedBy: string;
+}
+
+export interface Audit {
+    auditId: number
+    userId: number
+    actionType: string
+    entityId: number
+    actionTimestamp: string
+}
