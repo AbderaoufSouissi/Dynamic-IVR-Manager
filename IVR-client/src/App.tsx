76a3444e9febf4b0b7b8a3ef958c14083cdbe66b
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-07-22 07:32:27 +0100

    Frontend: Modification de la configuration de routage

--- IVR-client/src/App.tsx
+++ IVR-client/src/App.tsx
@@ -2,22 +2,42 @@
 import UserFilter from "./components/filters/UserFilter";
 import UsersTable from "./components/tables/UsersTable";
 import AdminDashboard from "./pages/AdminDashboard";
-import ForgetPasswordPage from "./pages/ForgetPasswordPage";
-import LoginPage from "./pages/login/LoginPage";
+import ForgetPasswordPage from "./auth/ForgetPasswordPage";
+import LoginPage from "./auth/LoginPage";
 import content from "./data/content.json"
 import PermissionsTable from "./components/tables/PermissionsTable";
+import { useEffect, useState } from "react";
+import { getUsers } from "./service/api";
+import MsisdnPage from "./pages/MsisdnPage";
 
 
 
 
 const App = () => {
+
+  // const [users, setUsers] = useState<any[]>([]);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const data = await getUsers();
+  //       setUsers(data);
+  //       console.log(users)
+  //     } catch (error) {
+  //       console.error("Failed to load users", error);
+  //     }
+  //   };
+
+  //   fetchUsers();
+  // }, []);
   return (
     <>
       {/* <ForgetPasswordPage/> */}
       {/* <LoginPage/> */}
       <AdminDashboard/>
+
       {/* <UsersTable users={content.users}/> */}
       {/* <PermissionsTable permissions={content.permissions}/> */}
+      {/* <MsisdnPage/> */}
     </>
   )
 }
--- IVR-client/src/main.tsx
+++ IVR-client/src/main.tsx
@@ -3,28 +3,61 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import LoginPage from './pages/login/LoginPage.tsx'
-import ForgetPasswordPage from './pages/ForgetPasswordPage.tsx'
+import LoginPage from './auth/LoginPage.tsx'
+import ForgetPasswordPage from './auth/ForgetPasswordPage.tsx'
+import UsersPage from './pages/UsersPage.tsx'
+import RolesPage from './pages/RolesPage.tsx'
+import PermissionsPage from './pages/PermissionsPage.tsx'
+import MsisdnPage from './pages/MsisdnPage.tsx'
+import AdminDashboard from './pages/AdminDashboard.tsx'
 
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: createElement(App)
+    element: createElement(App),
   },
-  
   {
     path: "/login",
-    element: createElement(LoginPage)
+    element: createElement(LoginPage),
   },
-  
   {
     path: "/forget-password",
-    element: createElement(ForgetPasswordPage)
+    element: createElement(ForgetPasswordPage),
   },
-  
-])
+  {
+    path: "/admin",
+    element: createElement(AdminDashboard), // Sidebar and layout
+    children: [
+      {
+        path: "users",
+        element: createElement(UsersPage),
+      },
+      {
+        path: "roles",
+        element: createElement(RolesPage),
+      },
+      {
+        path: "permissions",
+        element: createElement(PermissionsPage),
+      },
+      {
+        path: "msisdn",
+        element: createElement(MsisdnPage),
+      },
+      {
+        path: "auditLogs",
+        element: <p>Logs d’audit ici.</p>,
+      },
+      {
+        index: true, // /admin by default
+        element: <p>Bienvenue dans le tableau de bord admin.</p>,
+      },
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
