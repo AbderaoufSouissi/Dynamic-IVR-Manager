b8696418768f65722b88200621ab98ca46e9e4cb
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-22 14:53:34 +0200

    Frontend: Ajouter un dossier sideBar

--- IVR-client/src/components/sideBar/SideBar.tsx
+++ IVR-client/src/components/sideBar/SideBar.tsx
@@ -4,6 +4,8 @@ import { FaUserAlt, FaShieldAlt } from "react-icons/fa";
 import { HiOutlineKey } from "react-icons/hi2";
 import { FiFileText, FiPhone } from "react-icons/fi";
 import { NavLink } from 'react-router-dom';
+import { BiLogOut } from 'react-icons/bi';
+import { TbLayoutDashboard } from 'react-icons/tb';
 
 interface SidebarProps {
   activeTab: string;
@@ -23,16 +25,18 @@ const navItems: NavItem[] = [
   { id: 'permissions', label: 'Permissions', icon: HiOutlineKey, route: '/admin/permissions' },
   { id: 'auditLogs', label: 'Audit', icon: FiFileText, route: '/admin/auditLogs' },
   { id: 'msisdn', label: 'MSISDN', icon: FiPhone, route: '/admin/msisdn' },
+  { id: 'logout', label: 'Logout', icon: BiLogOut , route: '/' },
 ];
 
 const Sidebar = ({ activeTab, onTabChange }: SidebarProps) => {
   return (
     <aside className="flex flex-col w-64 bg-white border-r border-gray-200 min-h-screen shadow-sm">
       <div className="flex items-center justify-center h-16 px-6 border-b border-gray-200 bg-white">
-        <h1 className="text-xl font-bold text-gray-800">Tableau de Bord</h1>
+        <TbLayoutDashboard size={30} className='text-blue-700 mr-1' />
+        <NavLink className="text-xl font-bold text-gray-900" to={'/admin'}>Tableau de Bord</NavLink>
       </div>
 
-      <nav className="flex-1 p-4 space-y-1 bg-white">
+      <nav className="flex-1 p-4 space-y-1 bg-white ">
         {navItems.map(({ id, label, icon: Icon, route }) => {
           const isActive = activeTab === id;
           return (
@@ -40,10 +44,10 @@ const Sidebar = ({ activeTab, onTabChange }: SidebarProps) => {
               key={id}
               to={route || '#'}
               onClick={() => onTabChange(id)}
-              className={`flex items-center w-full gap-3 px-4 py-2 rounded-lg transition 
+              className={` flex items-center w-full gap-3 px-4 py-2 rounded-lg transition 
                 ${isActive
                   ? 'bg-blue-100 text-blue-600 shadow-sm'
-                  : 'text-gray-700 hover:bg-gray-100 hover:text-blue-600'}
+                  : 'text-gray-900 hover:bg-gray-100 hover:text-blue-600'}
               `}
             >
               <Icon size={20} className={isActive ? 'text-blue-600' : ''} />
