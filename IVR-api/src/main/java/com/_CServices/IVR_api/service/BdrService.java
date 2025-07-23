d6ba959b1e38ac3653b3e0339a773aa0eec4367d
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-23 11:54:50 +0200

    Backend: Implementation de la logique pour réinitialiser le nombre d'appels d'un MSISDN donné

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/service/BdrService.java
@@ -0,0 +1,43 @@
+package com._CServices.IVR_api.service;
+
+import com._CServices.IVR_api.dao.BdrRepository;
+import com._CServices.IVR_api.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BdrService {
+
+    private final BdrRepository bdrRepository;
+
+    @Transactional
+    public void resetNbCalls(String msisdn){
+
+        log.info("inside resetNbCalls()");
+        if(!bdrRepository.existsByMsisdn(msisdn)){
+           throw new ResourceNotFoundException("Msisdn: "+msisdn+" not found");
+        }
+
+        int updatedRows = bdrRepository.resetNbCalls(msisdn);
+        if(updatedRows == 0 ){
+            throw new IllegalArgumentException("No record found for MSISDN: " + msisdn);
+        }
+
+    }
+
+    public boolean areNbCallsZero(String msisdn){
+        if(!bdrRepository.existsByMsisdn(msisdn)){
+            throw new ResourceNotFoundException("Msisdn: "+msisdn+" not found");
+        }
+        return bdrRepository.areNbCallsZero(msisdn);
+
+
+    }
+
+
+
+}
