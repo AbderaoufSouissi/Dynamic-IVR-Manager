3d44155947ab92bbdd064a192febb515e6b94278
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-23 11:53:45 +0200

    Backend: Ajouter la couche DAO pour les tables reliés au MSISDN

---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/dao/BdrRepository.java
@@ -0,0 +1,33 @@
+package com._CServices.IVR_api.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BdrRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+
+
+    public int resetNbCalls(String msisdn){
+        String sql = "UPDATE CCADMIN.BDR SET NBAPPELS = 0, NBAPPELS_CC = 0 WHERE MSISDN = ?";
+        return jdbcTemplate.update(sql, msisdn);
+    }
+
+    public boolean existsByMsisdn(String msisdn) {
+        String sql = "SELECT COUNT(*) FROM CCADMIN.BDR WHERE MSISDN = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, msisdn);
+        return count != null && count > 0;
+    }
+
+    public boolean areNbCallsZero(String msisdn) {
+        String sql = "SELECT COUNT(*) FROM CCADMIN.BDR WHERE MSISDN = ? AND NBAPPELS = 0 AND NBAPPELS_CC = 0";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, msisdn);
+        return count != null && count > 0;
+    }
+
+
+
+}
---
+++ IVR-api/src/main/java/com/_CServices/IVR_api/dao/ClientsCrmHistRepository.java
@@ -0,0 +1,42 @@
+package com._CServices.IVR_api.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ClientsCrmHistRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public boolean existsByMsisdn(String msisdn) {
+        String sql = "SELECT COUNT(*) FROM CCADMIN.CLIENTS_CRM_HIST WHERE MSISDN = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, msisdn);
+        return count != null && count > 0;
+    }
+
+    public int blacklistMsisdn(String msisdn) {
+        String sql = "UPDATE CCADMIN.CLIENTS_CRM_HIST SET BLACKLIST = 1 WHERE MSISDN = ?";
+        return jdbcTemplate.update(sql, msisdn);
+    }
+
+    public int whitelistMsisdn(String msisdn) {
+        String sql = "UPDATE CCADMIN.CLIENTS_CRM_HIST SET BLACKLIST = 0 WHERE MSISDN = ?";
+        return jdbcTemplate.update(sql, msisdn);
+    }
+
+
+    public boolean isMsisdnBlacklisted(String msisdn) {
+        String sql = "SELECT COUNT(*) FROM CCADMIN.CLIENTS_CRM_HIST WHERE MSISDN = ? AND BLACKLIST = 1";
+
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, msisdn);
+        return count != null && count > 0;
+    }
+
+    public boolean isMsisdnWhitelisted(String msisdn) {
+        String sql = "SELECT COUNT(*) FROM CCADMIN.CLIENTS_CRM_HIST WHERE MSISDN = ? AND BLACKLIST = 0";
+
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, msisdn);
+        return count != null && count > 0;
+    }
+}
