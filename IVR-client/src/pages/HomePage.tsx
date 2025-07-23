75ce194ab0078134d9257cc37b325b8afa2ffbe4
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-23 14:09:04 +0200

    Frontend: Modification de la page HomePage

--- IVR-client/src/pages/HomePage.tsx
+++ IVR-client/src/pages/HomePage.tsx
@@ -1,10 +1,3 @@
-import { HiOutlineUserAdd } from "react-icons/hi";
-import { TbLayoutDashboard } from "react-icons/tb";
-import { NavLink } from "react-router-dom";
-import FeatureCard from "../components/FeatureCard";
-import { MdOutlineSecurity } from "react-icons/md";
-import { FiPhoneCall } from "react-icons/fi";
-import { FaFileAlt, FaKey } from "react-icons/fa";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
