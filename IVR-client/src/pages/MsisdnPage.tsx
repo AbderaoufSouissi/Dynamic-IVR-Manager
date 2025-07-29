6688cc842cd9d4492492b7c11b7686975a0e4df1
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-29 19:21:41 +0200

    Frontend: Modification de la page MsisdnPage

--- IVR-client/src/pages/MsisdnPage.tsx
+++ IVR-client/src/pages/MsisdnPage.tsx
@@ -5,8 +5,12 @@ import { StatusMessage } from "../components/StatusMessage";
 import Modal from "../components/modal/Modal";
 
 import { FiAlertTriangle } from "react-icons/fi";
-import { blacklistMsisdn, isBlacklisted, resetNbCalls, WhitelistMsisdn } from "../service/MsisdnService";
-
+import {
+  blacklistMsisdn,
+  isBlacklisted,
+  resetNbCalls,
+  WhitelistMsisdn,
+} from "../service/MsisdnService";
 
 const MsisdnPage = () => {
   const [msisdn, setMsisdn] = useState("");
@@ -15,175 +19,160 @@ const MsisdnPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [showBlacklistModal, setShowBlacklistModal] = useState(false);
   const [showWhitelistModal, setShowWhitelistModal] = useState(false);
-  const [showResetModal,setShowResetModal] = useState(false)
+  const [showResetModal, setShowResetModal] = useState(false);
 
   const validateMSISDN = (number: string) => /^\d{8,15}$/.test(number);
 
-  
-
   const blacklist = async () => {
-    const data = await blacklistMsisdn(msisdn)
-  
-    setStatus(data.message)
-      console.log("status of blacklist  : ",status)
-
-  }
+    setIsLoading(true);
+    try {
+      const data = await blacklistMsisdn(msisdn); //
+      setStatus(data.message);
+      console.log(status);
+    } catch (err) {
+      console.error(err);
+      setStatus("Erreur lors du blacklistage.");
+    } finally {
+      setIsLoading(false);
+      setShowBlacklistModal(false);
+    }
+  };
 
   const handleOnBlacklist = () => {
-  if (!validateMSISDN(msisdn)) {
-    setError("Le format du MSISDN est invalide.");
-    return;
-  }
-  setError("");
-  setShowBlacklistModal(true);
-};
-
-
+    if (!validateMSISDN(msisdn)) {
+      setError("Le format du MSISDN est invalide.");
+      return;
+    }
+    setError("");
+    setShowBlacklistModal(true);
+  };
 
   const whitelist = async () => {
-    const data = await WhitelistMsisdn(msisdn)
-    setStatus(data.message)
-        console.log("status of whitelist  : ",status)
-
-  }
+    setIsLoading(true);
+    try {
+      const data = await WhitelistMsisdn(msisdn); // ✅ fixed
+      setStatus(data.message);
+    } catch (err) {
+      console.error(err);
+      setStatus("Erreur lors du whitelistage.");
+    } finally {
+      setIsLoading(false);
+      setShowWhitelistModal(false);
+    }
+  };
 
   const handleOnWhitelist = () => {
-  if (!validateMSISDN(msisdn)) {
-    setError("Le format du MSISDN est invalide.");
-    return;
-  }
-  setError("");
-  setShowWhitelistModal(true);
-};
-
-
+    if (!validateMSISDN(msisdn)) {
+      setError("Le format du MSISDN est invalide.");
+      return;
+    }
+    setError("");
+    setShowWhitelistModal(true);
+  };
 
   const reset = async () => {
-    const data = await resetNbCalls(msisdn)
-    setStatus(data.message)
-    console.log("status of reset nb calls  : ",status)
+    setIsLoading(true);
+    try {
+      const data = await resetNbCalls(msisdn); // ✅ fixed
+      setStatus(data.message);
+      console.log(status);
+    } catch (err) {
+      console.error(err);
+      setStatus("Erreur lors de la réinitialisation du nombre d'appels.");
+    } finally {
+      setIsLoading(false);
+      setShowResetModal(false);
+    }
+  };
 
-  }
-
- const handleOnReset = () => {
-  if (!validateMSISDN(msisdn)) {
-    setError("Le format du MSISDN est invalide.");
-    return;
-  }
-  setError("");
-  setShowResetModal(true);
-};
-
+  const handleOnReset = () => {
+    if (!validateMSISDN(msisdn)) {
+      setError("Le format du MSISDN est invalide.");
+      return;
+    }
+    setError("");
+    setShowResetModal(true);
+  };
 
   const handleOnVerify = () => {
-    isMsisdnBlacklisted()
-  }
-
+    if (!validateMSISDN(msisdn)) {
+      setError("Le format du MSISDN est invalide.");
+      return;
+    }
+    setError("");
+    isMsisdnBlacklisted();
+  };
 
   const isMsisdnBlacklisted = async () => {
-    const data = await isBlacklisted(msisdn)
-    setStatus(data.message)
-    console.log("status of is blacklisted  : ",status)
-  }
+    try {
+      const data = await isBlacklisted({ msisdn }); // pass msisdn as an object with key 'msisdn'
+      setStatus(data.message);
+      console.log("status of is blacklisted  : ", data.message);
+    } catch (error) {
+      console.error(error);
+      setStatus("Erreur lors de la vérification du blacklistage.");
+    }
+  };  
 
   return (
-    <div>
-
-    
-    <main className="flex flex-1 items-center justify-center py-12">
-      
-      <div className="w-full max-w-md space-y-8 bg-white p-8 border border-gray-200 rounded-xl shadow transition-all duration-300 cursor-pointer hover:shadow-2xl hover:bg-white/80">
-
-        <div>
-          <h2 className="text-center text-3xl font-bold text-gray-900">Gérer MSISDN</h2>
-          <p className="mt-2 text-center text-sm">Entrez un numéro de téléphone pour le gérer.</p>
+   <div className="h-screen overflow-hidden">
+      <main className="flex flex-1 items-center justify-center py-12 h-full">
+        <div className="w-full max-w-md space-y-8 bg-white p-8 border border-gray-200 rounded-xl shadow transition-all duration-300 cursor-pointer hover:shadow-2xl hover:bg-white/80">
+          <div>
+            <h2 className="text-center text-3xl font-bold text-gray-900">
+              Gérer MSISDN
+            </h2>
+            <p className="mt-2 text-center text-sm">
+              Entrez un numéro de téléphone pour le gérer.
+            </p>
+          </div>
+          <MSISDNInput value={msisdn} onChange={setMsisdn} error={error} />
+          <ActionButtons
+            isLoading={isLoading}
+            onVerify={handleOnVerify}
+            onBlacklist={handleOnBlacklist}
+            onWhitelist={handleOnWhitelist}
+            onReset={handleOnReset}
+          />
+          <StatusMessage message={status} />
         </div>
-        <MSISDNInput value={msisdn} onChange={setMsisdn} error={error} />
-        <ActionButtons
-          isLoading={isLoading}
-          onVerify={handleOnVerify}
-          onBlacklist={handleOnBlacklist}
-  onWhitelist={handleOnWhitelist}
-  onReset={handleOnReset}
+
+        {/* Modals */}
+        <Modal
+          open={showBlacklistModal}
+          onClose={() => setShowBlacklistModal(false)}
+          icon={<FiAlertTriangle />}
+          title="Confirmer le blacklistage"
+          description={`Êtes-vous sûr de vouloir blacklister le numéro ${msisdn} ?`}
+          confirmLabel="Confirmer"
+          confirmType="danger"
+          onConfirm={blacklist}
         />
-        <StatusMessage message={status} />
-      </div>
 
-      {/* Modals */}
-      <Modal
-  open={showBlacklistModal}
-  onClose={() => setShowBlacklistModal(false)}
-  icon={<FiAlertTriangle />}
-  title="Confirmer le blacklistage"
-  description={`Êtes-vous sûr de vouloir blacklister le numéro ${msisdn} ?`}
-  confirmLabel="Confirmer"
-  confirmType="danger"
-  onConfirm={async () => {
-  setIsLoading(true);
-  try {
-    const data = await blacklistMsisdn(msisdn);
-    setStatus(data.message);
-  } catch (err) {
-    console.error(err);
-    setStatus("Erreur lors du blacklistage.");
-  } finally {
-    setIsLoading(false);
-    setShowBlacklistModal(false);
-  }
-}}
-/>
+        <Modal
+          open={showWhitelistModal}
+          onClose={() => setShowWhitelistModal(false)}
+          icon={<FiAlertTriangle />}
+          title="Confirmer le whitelistage"
+          description={`Êtes-vous sûr de vouloir whitelister ce numéro ${msisdn} ?`}
+          confirmLabel="Confirmer"
+          confirmType="primary"
+          onConfirm={whitelist}
+        />
 
-      <Modal
-  open={showWhitelistModal}
-  onClose={() => setShowWhitelistModal(false)}
-  icon={<FiAlertTriangle />}
-  title="Confirmer le whitelistage"
-  description={`Êtes-vous sûr de vouloir whitelister ce numéro ${msisdn} ?`}
-  confirmLabel="Confirmer"
-  confirmType="primary"
-  onConfirm={async () => {
-  setIsLoading(true);
-  try {
-    const data = await blacklistMsisdn(msisdn);
-    setStatus(data.message);
-  } catch (err) {
-    console.error(err);
-    setStatus("Erreur lors du whitelistage.");
-  } finally {
-    setIsLoading(false);
-    setShowWhitelistModal(false);
-  }
-}}
-/>
-
-      <Modal
-  open={showResetModal}
-  onClose={() => setShowResetModal(false)}
-  icon={<FiAlertTriangle />}
-  title="Réinitialiser le compteur"
-  description={`Êtes-vous sûr de vouloir réinitialiser le nombre d'appels pour ${msisdn} ?`}
-  confirmLabel="Confirmer"
-  confirmType="warning"
-  onConfirm={async () => {
-  setIsLoading(true);
-  try {
-    const data = await blacklistMsisdn(msisdn);
-    setStatus(data.message);
-  } catch (err) {
-    console.error(err);
-    setStatus("Erreur lors de la réinitialisation du nombre d'appels.");
-  } finally {
-    setIsLoading(false);
-    setShowResetModal(false);
-  }
-}}
-/>
-
+        <Modal
+          open={showResetModal}
+          onClose={() => setShowResetModal(false)}
+          icon={<FiAlertTriangle />}
+          title="Réinitialiser le compteur"
+          description={`Êtes-vous sûr de vouloir réinitialiser le nombre d'appels pour ${msisdn} ?`}
+          confirmLabel="Confirmer"
+          confirmType="warning"
+          onConfirm={reset}
+        />
       </main>
-      </div>
+    </div>
   );
-};
+}
 
 export default MsisdnPage;
-
-
--- IVR-client/src/service/MsisdnService.ts
+++ IVR-client/src/service/MsisdnService.ts
@@ -1,20 +1,20 @@
 import { axiosInstance } from "../api/Api"
 
 export const blacklistMsisdn = async (msisdn: string) => {
-    const response = await axiosInstance.put("/msisdn/blacklist", msisdn);
+  const response = await axiosInstance.put("/msisdn/blacklist", { msisdn });
     return response.data
 
     
 }
-export const WhitelistMsisdn = async (msisdn: string) => {
-    const response = await axiosInstance.put("/msisdn/whitelist", msisdn);
-    return response.data
+  export const WhitelistMsisdn = async (msisdn: string) => {
+    const response = await axiosInstance.put("/msisdn/whitelist", { msisdn });
+      return response.data
 
-    
-}
+      
+  }
 
 export const resetNbCalls = async (msisdn: string) => {
-    const response = await axiosInstance.put("/msisdn/reset", msisdn);
+  const response = await axiosInstance.put("/msisdn/reset", { msisdn });
     return response.data
 
     
