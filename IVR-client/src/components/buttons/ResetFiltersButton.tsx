0f56d20b31d57ffb6f97abc59a66c328c2695965
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-29 11:53:20 +0200

    Frontend: Remplace le component FilterButtons par le component ResetFiltersButton

---
+++ IVR-client/src/components/buttons/ResetFiltersButton.tsx
@@ -0,0 +1,14 @@
+const ResetFiltersButton = () => {
+  return (
+    <div className="md:col-span-3 flex gap-4 mt-4">
+          <button
+            className="cursor-pointer bg-gray-200 py-3 px-6 rounded-xl text-blue-600 font-semibold hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200 transform hover:scale-[1.02] active:scale-[0.98] disabled:transform-none disabled:cursor-not-allowed shadow-lg hover:shadow-xl"
+            type="button"
+          >
+            Réinitialiser les filtres
+          </button>
+        </div>
+  );
+};
+
+export default ResetFiltersButton;
