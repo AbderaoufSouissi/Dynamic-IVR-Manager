a4a4cc94ce8f1e84e14466aadeb37ab7b943c5ce
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-30 15:05:39 +0200

    Backend: Modification de la methode countAuditsWithFilters()

--- IVR-api/src/main/java/com/_CServices/IVR_api/dao/AuditRepository.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/dao/AuditRepository.java
@@ -20,7 +20,7 @@ public interface AuditRepository extends JpaRepository<Audit, Long>, JpaSpecific
     WHERE (:auditId IS NULL OR audit_id = :auditId)
       AND (:userId IS NULL OR user_id = :userId)
       AND (:entityId IS NULL OR entity_id = :entityId)
-      AND (:msisdn IS NULL OR msisdn = :msis    dn)
+      AND (:msisdn IS NULL OR msisdn = :msisdn)
       AND (:actionType IS NULL OR UPPER(action_type) = UPPER(:actionType))
       AND (:entityType IS NULL OR UPPER(entity_type) = UPPER(:entityType))
       AND (:startTimestamp IS NULL OR action_time_stamp >= :startTimestamp)
