9733cd4a24a92c12c9fe8912b33a2e9e6002c92d
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-07-30 10:57:11 +0200

    Frontend: Modification du style des buttons des formulaires:
    - UserForm
    - RoleForm
    - PermissionForm

--- IVR-client/src/components/forms/UserForm.tsx
+++ IVR-client/src/components/forms/UserForm.tsx
@@ -307,7 +307,7 @@ if (formData.active !== null) {
 
             <footer className="mt-6 flex justify-between">
               <button
-                className="px-4 cursor-pointer py-2 bg-red-700 text-white font-medium rounded-lg hover:bg-red-800 transition text-sm"
+className="px-5 py-2 font-semibold hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 text-sm cursor-pointer bg-gradient-to-r from-red-600 to-red-700 hover:from-red-700 hover:to-red-800 disabled:from-gray-400 disabled:to-gray-500 text-white rounded-xl transition-all duration-200 focus:ring-red-500/20 transform hover:scale-[1.02] active:scale-[0.98] disabled:transform-none disabled:cursor-not-allowed shadow-lg"
                 type="button"
                 onClick={handleCancel}
               >
@@ -315,7 +315,7 @@ if (formData.active !== null) {
               </button>
               <button
                 onClick={handleSubmit}
-                className="px-5 cursor-pointer py-2 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition text-sm"
+                  className="px-5 py-2  font-semibold  hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 text-sm  cursor-pointer bg-gradient-to-r from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800 disabled:from-gray-400 disabled:to-gray-500 text-white rounded-xl transition-all duration-200  focus:ring-blue-500/20 transform hover:scale-[1.02] active:scale-[0.98] disabled:transform-none disabled:cursor-not-allowed shadow-lg"
                 type="submit"
               >
                 Valider
