0bf84cba15393e0cfb800c8e9487e9dc9eab1b95
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-08-03 17:18:33 +0100

    Frontend: Modification du style des notifications Toastify

--- IVR-client/src/service/ToastService.ts
+++ IVR-client/src/service/ToastService.ts
@@ -1,7 +1,7 @@
 import { toast, type ToastOptions } from "react-toastify";
 
 const toastConfig: ToastOptions = {
-  position: "bottom-left",
+  position: "top-right",
   autoClose: 2000,
   hideProgressBar: false,
   closeOnClick: true,
@@ -9,8 +9,24 @@ const toastConfig: ToastOptions = {
   draggable: true,
   progress: undefined,
   theme: "light",
+  className: "font-sans",
+  style: {
+    background: '#ffffff',   // Tailwind blue-600
+    color: '#18181b',
+  
+    fontWeight: 500,
+    fontSize: '14px',
+    borderRadius: '8px',
+    padding: '12px 16px',
+    boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+  },
+
+  progressClassName: "custom-toast-progress"
   
 };
+
+
+
 
 export const toastInfo = (message: string) => {
   toast.info(message, toastConfig);
