c979d389d9a3ea1a433216aef04dcbac2b41662a
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-08-05 12:56:02 +0200

    Backend: Modification du controller FrontendController pour permettre l'accés aux endpoints frontend

--- IVR-api/src/main/java/com/_CServices/IVR_api/controller/FrontendController.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/controller/FrontendController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = {"/", "/{path:^(?!api$|static$)[^\\.]*}"})
+    @RequestMapping(value = {"/{path:^(?!api$|static$)[^\\.]*}","/",
+            "/login",
+            "/forget-password",
+            "/reset-password/**",
+            "/admin",
+            "/admin/**"})
     public String redirect() {
         return "forward:/index.html";
     }
