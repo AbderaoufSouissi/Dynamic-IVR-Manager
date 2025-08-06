78a844873b7e3a168c3f51ed55b263da7a2ddc32
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-08-06 16:58:32 +0200

    Frontend: Modification du style du component Header

--- IVR-client/src/components/header/Header.tsx
+++ IVR-client/src/components/header/Header.tsx
@@ -9,17 +9,20 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
             <TbLayoutDashboard size={40} className="text-blue-700" />
-            <h2 className="text-xl font-bold text-gray-900">Dynamic IVR Manager</h2>
+            <h2 className="text-xl font-medium text-gray-900">
+              Dynamic IVR Manager
+            </h2>
           </div>
-
-          <nav className="absolute left-1/2 transform -translate-x-1/2">
-            <a
-              className="text-gray-600 hover:text-blue-600 font-medium transition"
-              href="#features"
-            >
-              Fonctionnalités
-            </a>
-          </nav>
+          <div>
+            <nav className="absolute inset-0 flex items-center justify-center">
+              <a
+                className="text-gray-600 hover:text-blue-600 font-medium transition-colors duration-200 px-3 py-1 rounded-md hover:bg-gray-50"
+                href="#features"
+              >
+                Fonctionnalités
+              </a>
+            </nav>
+          </div>
 
           <div className="flex items-center justify-between">
             <a href="#" className="text-sm/6 font-semibold text-gray-900"></a>
