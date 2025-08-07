e71e2d33f8039976282bf24c290c9493b4f21b83
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-08-07 17:23:09 +0200

    frontend: Modification du component ProfileInput

--- IVR-client/src/components/inputs/ProfileInput.tsx
+++ IVR-client/src/components/inputs/ProfileInput.tsx
@@ -3,13 +3,13 @@ const ProfileInput = ({ label, value, id }: { label: string; value?: string; id:
     <label htmlFor={id} className="block text-sm/6 font-medium text-gray-900">
       {label}
     </label>
-    <div className="mt-2">
+    <div className="mt-1">
       <input
         id={id}
         type="text"
         value={value || ""}
         readOnly
-        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+        className="block w-full rounded-md bg-white px-3 py-0.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
       />
     </div>
   </div>
