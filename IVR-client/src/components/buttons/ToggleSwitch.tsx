bebce660b679569746154992b8ae1ed44337740d
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-08-08 11:56:34 +0200

    Frontend: Modification du button ToggleSwitch

--- IVR-client/src/components/buttons/ToggleSwitch.tsx
+++ IVR-client/src/components/buttons/ToggleSwitch.tsx
@@ -8,22 +8,23 @@ const ToggleSwitch = ({ checked, onToggle }: ToggleSwitchProps) => {
     <div className="flex items-center justify-center">
       <button
         onClick={onToggle}
-        className={`cursor-pointer relative inline-flex h-5 w-10 items-center rounded-full transition-all duration-200 ease-in-out ring-2 ring-offset-2 ${
+        className={`cursor-pointer relative inline-flex h-5 w-11 items-center rounded-full transition-all duration-200 ease-in-out ring-2 ring-offset-2 ${
           checked
-            ? 'bg-blue-600 ring-blue-500'  // Checked: blue bg + blue ring
-            : 'bg-gray-200 ring-gray-400' // Unchecked: gray bg + gray ring
+            ? 'bg-blue-600 ring-blue-500'
+            : 'bg-gray-200 ring-gray-400'
         }`}
         role="switch"
         aria-checked={checked}
       >
         <span
           className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-200 ease-in-out ${
-            checked ? 'translate-x-6' : 'translate-x-1'
+            checked ? 'translate-x-6.5' : 'translate-x-1'
           }`}
         />
       </button>
     </div>
   );
 };
+
 
 export default ToggleSwitch;
