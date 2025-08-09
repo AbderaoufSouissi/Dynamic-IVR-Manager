425fc1a50a76d96fb3d76deb9f502130c9421335
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-08-09 09:31:11 +0100

    Frontend: Modification du Hook useAuth

--- IVR-client/src/hooks/useAuth.ts
+++ IVR-client/src/hooks/useAuth.ts
@@ -1,21 +1,20 @@
 // hooks/useAuth.ts
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { getCurrentUser } from "../service/AuthService";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const useAuth = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/auth/user`, { withCredentials: true }) // adjust to your API
-      .then(res => {
-        setUser(res.data) 
-      })
-      .catch(() => setUser(null))
-      .finally(() => setLoading(false));
-  }, []);
+  getCurrentUser()
+    .then(userData => {
+      setUser(userData);
+    })
+    .catch(() => setUser(null))
+    .finally(() => setLoading(false));
+}, []);
 
-  return { user, loading };
+return { user, loading };
 };
