ca6f243729b53c319922261aa97812a61041f69f
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-08-11 15:45:27 +0200

    Backend: Modification du type de fichier du build de .jar vers .war

--- IVR-api/src/main/java/com/_CServices/IVR_api/Application.java
+++ IVR-api/src/main/java/com/_CServices/IVR_api/Application.java
@@ -1,13 +1,23 @@
 package com._CServices.IVR_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
 }
